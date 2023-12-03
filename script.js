9070d350f1ab643a20d2a83934d8fd72d620e2f5
Austin Luper <austin.luper@gmail.com>
2023-12-03 18:05:09 -0500

    Added a var userInstructions into js file as a p HTML element, with user instructions, as well as comment describing the added code

--- script.js
+++ script.js
@@ -3,6 +3,13 @@
 //Creates HTML form element
 var formSection = document.createElement("form");
 
+//Creates a HTML p element and user instructions informing user they will need to input appropriate
+//answers if they want a random password to be generated.
+var userInstructions = document.createElement("p")
+  userInstructions.textContent = `In order for this application to generate a secure, random password, 
+  you must appropriately answer the following password requirement prompts.
+ `;
+ 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
